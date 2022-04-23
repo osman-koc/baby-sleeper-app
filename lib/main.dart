f63d600ec26452810858bef86f19c3efd6d6aae3
Osman Koç <info@osmkoc.com>
2022-04-24 01:41:08 +0300

    floatingActionButton added

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:babysleeper/constants/const_asset.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:number_inc_dec/number_inc_dec.dart';
 import 'package:path_provider/path_provider.dart';
 import 'constants/const_voice.dart';
 
@@ -72,24 +73,6 @@ class _MyHomePageState extends State<MyHomePage> {
     isPlaying = audioPlayer.state == PlayerState.PLAYING;
   }
 
-  Future playLocal(localFileName) async {
-    if (!isPlaying) {
-      await audioPlayer.stop();
-    } else {
-      final dir = await getApplicationDocumentsDirectory();
-      final file = File('${dir.path}/$localFileName');
-      if (!(await file.exists())) {
-        final soundData =
-            await rootBundle.load(ConstAsset.getAudioLocalPath(localFileName));
-        final bytes = soundData.buffer.asUint8List();
-        await file.writeAsBytes(bytes, flush: true);
-      }
-      await audioPlayer.setUrl(file.path, isLocal: true);
-      await audioPlayer.setReleaseMode(ReleaseMode.LOOP);
-      await audioPlayer.resume();
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     List<String> voiceNames = <String>[
@@ -105,6 +88,7 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(AppLocalizations.of(context).translate('appName')),
       ),
+      //floatingActionButton: playTimerModalButton(context),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -120,96 +104,169 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
+  /* FILE METHODS */
+  Future playLocal(localFileName) async {
+    if (!isPlaying) {
+      await audioPlayer.stop();
+    } else {
+      String filePath = await getSoundFilePath(localFileName);
+      await audioPlayer.setUrl(filePath, isLocal: true);
+      await audioPlayer.setReleaseMode(ReleaseMode.LOOP);
+      await audioPlayer.resume();
+    }
+  }
+
+  Future<String> getSoundFilePath(localFileName) async {
+    final dir = await getApplicationDocumentsDirectory();
+    final file = File('${dir.path}/$localFileName');
+    if (!(await file.exists())) {
+      await fileWriteAsBytes(localFileName, file);
+    }
+    return file.path;
+  }
+
+  Future<void> fileWriteAsBytes(localFileName, File file) async {
+    final soundData =
+        await rootBundle.load(ConstAsset.getAudioLocalPath(localFileName));
+    final bytes = soundData.buffer.asUint8List();
+    await file.writeAsBytes(bytes, flush: true);
+  }
+  /* FILE METHODS */
+
+  /* THEME METHODS */
   Text footerText(BuildContext context) {
     return Text(
-            AppLocalizations.of(context).translate('copyright'),
-            textAlign: TextAlign.center,
-            style: const TextStyle(
-              color: Colors.black87,
-              fontSize: 10.0,
-            ),
-          );
+      AppLocalizations.of(context).translate('copyright'),
+      textAlign: TextAlign.center,
+      style: const TextStyle(
+        color: Colors.black87,
+        fontSize: 10.0,
+      ),
+    );
   }
 
   Text descriptionTexts(BuildContext context) {
     return Text(
-            '\n\n' +
-                AppLocalizations.of(context).translate('pressForHelp') +
-                '\n' +
-                AppLocalizations.of(context).translate('pressForStop'),
-            textAlign: TextAlign.center,
-            style: const TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
-          );
+      '\n\n' +
+          AppLocalizations.of(context).translate('pressForHelp') +
+          '\n' +
+          AppLocalizations.of(context).translate('pressForStop'),
+      textAlign: TextAlign.center,
+      style: const TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
+    );
   }
 
   Container audioDropdown(List<String> voiceNames) {
     return Container(
-            padding: const EdgeInsets.all(5.0),
-            child: DropdownButton<String>(
-              value: voiceNames[selectedVoiceIndex],
-              items: voiceNames.map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value, style: const TextStyle(fontSize: 16)),
-                );
-              }).toList(),
-              onChanged: (value) {
-                setState(() {
-                  selectedVoiceIndex = voiceNames.indexOf(value!);
-                  isPlaying = false;
-                  playLocal(ConstVoice.getAll[selectedVoiceIndex])
-                      .then((value) => (null));
-                });
-              },
-              style: const TextStyle(color: Colors.black87, fontSize: 20),
-            ),
+      padding: const EdgeInsets.all(5.0),
+      child: DropdownButton<String>(
+        value: voiceNames[selectedVoiceIndex],
+        items: voiceNames.map<DropdownMenuItem<String>>((String value) {
+          return DropdownMenuItem<String>(
+            value: value,
+            child: Text(value, style: const TextStyle(fontSize: 16)),
           );
+        }).toList(),
+        onChanged: (value) {
+          setState(() {
+            selectedVoiceIndex = voiceNames.indexOf(value!);
+            isPlaying = false;
+            playLocal(ConstVoice.getAll[selectedVoiceIndex])
+                .then((value) => (null));
+          });
+        },
+        style: const TextStyle(color: Colors.black87, fontSize: 20),
+      ),
+    );
+  }
+
+  FloatingActionButton playTimerModalButton(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: () {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text('Close after this time (min)'),
+              content: inputMinuteForTimerInModal(context),
+              actions: <Widget>[
+                TextButton(
+                  child: const Text('Reset'),
+                  onPressed: () {
+                    _numberInput.text = '0';
+                  },
+                ),
+                TextButton(
+                  child: const Text('OK'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            );
+          },
+        );
+      },
+      child: const Icon(Icons.timer),
+      tooltip: 'Set Timer',
+    );
+  }
+
+  final _numberInput = TextEditingController(text: '0');
+
+  Widget inputMinuteForTimerInModal(BuildContext context) {
+    return NumberInputWithIncrementDecrement(
+        controller: _numberInput,
+        scaleWidth: 1,
+        scaleHeight: 1,
+        initialValue: 0,
+        min: 0,
+        isInt: true);
   }
 
   ButtonTheme playButton() {
     return ButtonTheme(
-            padding: const EdgeInsets.all(30.0),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(100),
-            ),
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                primary: Colors.blueGrey,
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 35, vertical: 20),
-              ),
-              onPressed: buttonClickEvent,
-              child: buttonContent(),
-            ),
-          );
+      padding: const EdgeInsets.all(30.0),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(100),
+      ),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          primary: Colors.blueGrey,
+          padding: const EdgeInsets.symmetric(horizontal: 35, vertical: 20),
+        ),
+        onPressed: buttonClickEvent,
+        child: buttonContent(),
+      ),
+    );
   }
 
   Column buttonContent() {
     return Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset(getButtonImage(), width: 160),
-                  const Padding(padding: EdgeInsets.all(8)),
-                  Text(
-                    getButtonText(),
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(color: Colors.white, fontSize: 18),
-                  )
-                ],
-              );
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset(getButtonImage(), width: 160),
+        const Padding(padding: EdgeInsets.all(8)),
+        Text(
+          getButtonText(),
+          textAlign: TextAlign.center,
+          style: const TextStyle(color: Colors.white, fontSize: 18),
+        )
+      ],
+    );
   }
 
-  void buttonClickEvent() => setState(() {
-                isPlaying = !isPlaying;
-                playLocal(ConstVoice.getAll[selectedVoiceIndex])
-                    .then((value) => (null));
-              });
-
   String getButtonImage() => isPlaying ? imgActive : imgPassive;
-
   String getButtonText() {
     return isPlaying
         ? AppLocalizations.of(context).translate('buttonTextStop')
         : AppLocalizations.of(context).translate('buttonTextPlay');
   }
+
+  void buttonClickEvent() => setState(() {
+        isPlaying = !isPlaying;
+        playLocal(ConstVoice.getAll[selectedVoiceIndex])
+            .then((value) => (null));
+      });
+  /* THEME METHODS */
 }
